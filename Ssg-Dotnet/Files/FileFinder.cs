c5927e9f360a4cff126874e9c7c761b21f984db6
Asbjørn Brandt <asbjoernbrandt@gmail.com>
2023-01-15 15:57:50 +0100

    Add basic functionality for enumerating files

---
+++ Ssg-Dotnet/Files/FileFinder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Ssg_Dotnet.Files;
+internal static class FileFinder
+{
+    public static IEnumerable<FilePath> FindFiles(string directoryPath, string extension)
+    {
+        var files = Directory.GetFiles(directoryPath, $"*{extension}", SearchOption.AllDirectories);
+        return files.Select(file => new FilePath(file));
+    }
+
+    public static IEnumerable<FilePath> FindFiles(string directoryPath)
+    {
+        var files = Directory.GetFiles(directoryPath, "*.*", SearchOption.AllDirectories);
+        return files.Select(file => new FilePath(file));
+    }
+}
