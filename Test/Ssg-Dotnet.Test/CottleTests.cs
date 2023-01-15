5e15736f0d552e30b5cc13143b459e88c18c7b21
Asbjørn Brandt <asbjoernbrandt@gmail.com>
2023-01-15 14:42:40 +0100

    Test cottle

---
+++ Test/Ssg-Dotnet.Test/CottleTests.cs
@@ -0,0 +1,35 @@
+using Cottle;
+
+namespace Ssg_Dotnet.Test;
+//Some exploration of the Cottle library to assert that it supports the features wanted
+[TestFixture, Parallelizable(ParallelScope.Self)]
+public class CottleTests
+{
+    [Test]
+    public void ShouldReplace()
+    {
+        const string template = "<main>{content}</main>";
+        var document = Document.CreateDefault(template).DocumentOrThrow;
+        var result = document.Render(Context.CreateBuiltin(new Dictionary<Value, Value>
+        {
+            ["content"] = "<h1>Hello World</h1>"
+        }));
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result, Is.EqualTo("<main><h1>Hello World</h1></main>"));
+    }
+
+    [Test]
+    public void ShouldReplaceButKeepWhitespace()
+    {
+        const string template = "<main>\n        {content}\n    </main>\n";
+        var configuration = new DocumentConfiguration { Trimmer = DocumentConfiguration.TrimNothing };
+        var document = Document.CreateDefault(template, configuration).DocumentOrThrow;
+        var result = document.Render(
+            Context.CreateBuiltin(new Dictionary<Value, Value>
+        {
+            ["content"] = "<h1>Hello World</h1>"
+        }));
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result, Is.EqualTo("<main>\n        <h1>Hello World</h1>\n    </main>\n"));
+    }
+}
