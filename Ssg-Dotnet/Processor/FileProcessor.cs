2be52f0c4dac883723fdac0348162e3f91d47d58
Asbjørn Brandt <asbjoernbrandt@gmail.com>
2023-01-20 22:01:45 +0100

    Simplify a bit

--- Ssg-Dotnet/Processor/FileProcessor.cs
+++ Ssg-Dotnet/Processor/FileProcessor.cs
@@ -21,44 +21,40 @@ internal class FileProcessor
         await templateHandler.PrepareLayouts(layoutfolder);
         var inputFileHandler = new InputFileHandler(inputFolder);
         var outputFileHandler = new OutputFileHandler(inputFolder, outputFolder);
+        await ProcessFolder(inputFileHandler, outputFileHandler, isNote: false);
         if (notesFolder != null)
         {
             var notesFileHandler = new InputFileHandler(notesFolder);
             var notesOutputHandler = new OutputFileHandler(notesFolder, Path.Combine(outputFolder, "notes"));
-            foreach (var file in notesFileHandler.FindFiles())
-            {
-                var filePath = FilePath.FromString(file);
-                if (filePath.Extension == ".md")
-                {
-                    var input = await notesFileHandler.ReadFileAsync(file);
-                    var content = Markdown.ToHtml(input);
-                    //switch extention to .html for outputFile:
-                    var outputFile = filePath.ToIndexHtml();
-                    var output = templateHandler.RenderNote("default", filePath.FileName, content, new List<string>());
-                    await notesOutputHandler.WriteFileAsync(outputFile.RelativePath, output);
-                }
-                else
-                {
-                    notesOutputHandler.CopyFile(file);
-                }
-            }
+            await ProcessFolder(notesFileHandler, notesOutputHandler, isNote: true);
         }
+    }
 
-        foreach (var file in inputFileHandler.FindFiles())
+    private async Task ProcessFolder(InputFileHandler inputHandler, OutputFileHandler outputHandler, bool isNote)
+    {
+        foreach (var file in inputHandler.FindFiles())
         {
             var filePath = FilePath.FromString(file);
             if (filePath.Extension == ".md")
             {
-                var input = await inputFileHandler.ReadFileAsync(file);
+                var input = await inputHandler.ReadFileAsync(file);
                 var content = Markdown.ToHtml(input);
                 //switch extention to .html for outputFile:
                 var outputFile = filePath.ToIndexHtml();
-                var output = templateHandler.Render("default", filePath.FileName, content);
-                await outputFileHandler.WriteFileAsync(outputFile.RelativePath, output);
+                string output;
+                if (isNote)
+                {
+                    output = templateHandler.RenderNote("default", filePath.FileName, content, new List<string>());
+                }
+                else
+                {
+                    output = templateHandler.Render("default", filePath.FileName, content);
+                }
+                await outputHandler.WriteFileAsync(outputFile.RelativePath, output);
             }
             else
             {
-                outputFileHandler.CopyFile(file);
+                outputHandler.CopyFile(file);
             }
         }
     }
