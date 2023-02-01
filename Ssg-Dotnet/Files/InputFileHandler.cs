b03d4d9404b763cc35814e348b7c289aa40d8d4b
Asbjørn Brandt <asbjoernbrandt@gmail.com>
2023-02-01 22:01:46 +0100

    Add basic, naive filter

--- Ssg-Dotnet/Files/InputFileHandler.cs
+++ Ssg-Dotnet/Files/InputFileHandler.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 namespace Ssg_Dotnet.Files;
 internal class InputFileHandler
 {
+    private readonly List<string> filters = new() { ".vscode", ".git", ".gitignore", ".noncontent" };
+
     private readonly string inputFolder;
 
     public InputFileHandler(string inputFolder)
@@ -18,18 +20,41 @@ internal class InputFileHandler
     public IEnumerable<FilePath> FindFiles(string extension)
     {
         var files = Directory.EnumerateFiles(inputFolder, $"*{extension}", SearchOption.AllDirectories);
-        return files.Select(x => FilePath.FromFullPath(x, inputFolder));
+        var filteredFiles = Filter(files);
+        return filteredFiles.Select(x => FilePath.FromFullPath(x, inputFolder));
     }
 
     //Returns relative paths to files in the input folder
     public IEnumerable<FilePath> FindFiles()
     {
         var files = Directory.EnumerateFiles(inputFolder, "*.*", SearchOption.AllDirectories);
-        return files.Select(x => FilePath.FromFullPath(x, inputFolder));
+        var filteredFiles = Filter(files);
+        return filteredFiles.Select(x => FilePath.FromFullPath(x, inputFolder));
     }
 
     public static async Task<string> ReadFileAsync(FilePath path)
     {
         return await File.ReadAllTextAsync(path.AbsolutePath);
+    }
+
+    private IEnumerable<string> Filter(IEnumerable<string> files)
+    {
+        //Use hardcoded filter for now - should be able to use .gitignore or other more dynamic filtering at some point
+        foreach (var file in files)
+        {
+            var ignore = false;
+            foreach (var filter in filters)
+            {
+                if (file.Contains(filter))
+                {
+                    ignore = true;
+                    break;
+                }
+            }
+            if (!ignore)
+            {
+                yield return file;
+            }
+        }
     }
 }
